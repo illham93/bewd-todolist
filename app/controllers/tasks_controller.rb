1771fe8708f73e9cc3508215850fbf994eac7c02
Devon <devonhamill93@gmail.com>
2024-06-27 15:49:24 -0400

    Add tasks complete

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,19 @@
 class TasksController < ApplicationController
+    def index
+        @tasks = Task.all
+        render 'tasks/index'
+    end
+
+    def create
+        @task = Task.new(task_params)
+
+        if @task.save
+            render 'tasks/create'
+        end
+    end
+
+    private
+        def task_params
+            params.require(:task).permit(:content)
+        end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
 
   # Add routes below this line
 
+  get 'tasks' => 'tasks#index'
+
+  post 'tasks' => 'tasks#create'
+
   # Add routes below above line
 
   # Redirect all other paths to index page, which will be taken over by AngularJS
