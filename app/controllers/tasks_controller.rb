420ddf9034fe07f927845c5ffc7aa758612f256c
Devon <devonhamill93@gmail.com>
2024-06-28 14:03:46 -0400

    6.4 Complete

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -12,6 +12,32 @@ class TasksController < ApplicationController
         end
     end
 
+    def destroy
+        @task = Task.find_by(id: params[:id])
+
+        if @task and @task.destroy
+            render json: {success: true}
+        else
+            render json: {success: false}
+        end
+    end
+
+    def mark_complete
+        @task = Task.find_by(id: params[:id])
+
+        if @task and @task.update(completed: true)
+            render 'tasks/update'
+        end
+    end
+
+    def mark_active
+        @task = Task.find_by(id: params[:id])
+
+        if @task and @task.update(completed: false)
+            render 'tasks/update'
+        end
+    end
+
     private
         def task_params
             params.require(:task).permit(:content)
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,12 @@ Rails.application.routes.draw do
 
   post 'tasks' => 'tasks#create'
 
+  delete 'tasks/:id' => 'tasks#destroy'
+
+  put 'tasks/:id/mark_complete' => 'tasks#mark_complete'
+
+  put 'tasks/:id/mark_active' => 'tasks#mark_active'
+
   # Add routes below above line
 
   # Redirect all other paths to index page, which will be taken over by AngularJS
